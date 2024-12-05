a680a2133f29ee607db1222d3a4dde9a96009c75
Rana Zohaib Zaman <zohaibzaman28@gmail.com>
2024-12-05 19:12:19 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+alert("Hi Zohaib");
+alert(true)
+alert
+//Data Type
+//" " string 
+// number int 333 -333456
+
+stdobtMarks=  prompt("Enter Your Name")
+alert(a)
+
+Document.write(a)
+
+
+// uint 34567897689.57689
+// bool true false 
